41c37a7fec91527e6d29c29876f185a5b0e39455
MEKONGO ABANDA Yannick Edouard <mekongoabanda21@gmail.com>
2020-04-24 00:11:27 +0100

    tabBar custom

---
+++ MonRestauDuMonde/MonRestauDuMonde/Controlleur/MonTabBar.swift
@@ -0,0 +1,27 @@
+//
+//  MonTabBar.swift
+//  MonRestauDuMonde
+//
+//  Created by user159565 on 4/23/20.
+//  Copyright © 2020 user159565. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Couleur de notre tabBar
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        //couleur des icône de la tabBar
+        tabBar.tintColor = GRIS_TRES_CLAIR
+        
+        
+        
+    }
+    
+
+
+}
---
+++ MonRestauDuMonde/MonRestauDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,18 @@
+//
+//  Constantes.swift
+//  MonRestauDuMonde
+//
+//  Created by user159565 on 4/23/20.
+//  Copyright © 2020 user159565. All rights reserved.
+//
+
+//------- CE FICHIERS SWIFT NOUS PERMET DE DEFINIR CERTAINES COULEURS ET LES APPELER DANS DIFFERENTES CLASSES
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
+let VERT_WEM = UIColor(red: 50, green: 104, blue: 52, alpha: 1)
+let OR = UIColor(red: 255, green: 204, blue: 0, alpha: 1)
+
