44aa1de3fc64005968619901e9f44c71707af714
MEKONGO ABANDA Yannick Edouard <mekongoabanda21@gmail.com>
2020-04-23 23:17:49 +0100

    Ajout des controlleurs principaux

---
+++ MonRestauDuMonde/MonRestauDuMonde/Controlleur/MenuController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuController.swift
+//  MonRestauDuMonde
+//
+//  Created by user159565 on 4/23/20.
+//  Copyright © 2020 user159565. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
